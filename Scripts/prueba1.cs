81f4b350c247aafc09dd60c18626b65acb117290
Diego Resendiz Ojeda <di_564@hotmail.com>
2020-01-31 14:56:25 -0800

    Update prueba1.cs

--- Scripts/prueba1.cs
+++ Scripts/prueba1.cs
@@ -7,7 +7,7 @@ public class prueba1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+     //prueba de sincronizacion   
     }
 
     // Update is called once per frame
